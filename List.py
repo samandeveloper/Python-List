6b95270fe4fdf41ae3e55eee10ee98a007b383de
samandeveloper <sn.mohaghegh@gmail.com>
2020-10-05 12:25:40 -0600

    change the solution

--- List.py
+++ List.py
@@ -1,11 +1,8 @@
-#Questions:
-
 # using this list, 
 basket = ["Banana", "Apples", "Oranges", "Blueberries"]
 
 # # 1. Remove the Banana from the list
 basket.remove("Banana")
-
 # # 2. Remove "Blueberries" from the list.
 basket.remove('Blueberries')
 # # 3. Put "Kiwi" at the end of the list.
@@ -17,12 +14,3 @@ basket.count('Apples')
 # # 6. empty the basket
 basket.clear()
 print(basket)
-
-
-# Solution:
-1 - basket.remove('Banana')
-2 - basket.pop()
-3 - basket.append('Kiwi')
-4 - basket.insert(0, 'Apples')
-5 - basket.count('Apples')
-6 - basket.clear()
