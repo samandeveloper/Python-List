7e57625cf9e0c9381cdae82a6e4555ee9876a2c3
samandeveloper <sn.mohaghegh@gmail.com>
2020-10-05 12:04:16 -0600

    commit python-list

---
+++ List.py
@@ -0,0 +1,28 @@
+#Questions:
+
+# using this list, 
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+
+# # 1. Remove the Banana from the list
+basket.remove("Banana")
+
+# # 2. Remove "Blueberries" from the list.
+basket.remove('Blueberries')
+# # 3. Put "Kiwi" at the end of the list.
+basket.extend(['Kiwi'])
+# # # 4. Add "Apples" at the beginning of the list
+basket.insert(0,'Apples')
+# # 5. Count how many apples in the basket
+basket.count('Apples')
+# # 6. empty the basket
+basket.clear()
+print(basket)
+
+
+# Solution:
+1 - basket.remove('Banana')
+2 - basket.pop()
+3 - basket.append('Kiwi')
+4 - basket.insert(0, 'Apples')
+5 - basket.count('Apples')
+6 - basket.clear()
